a825696033680306ac7fea5991a0eec9c29e59f8
Camilo Figueroa <cigotete@gmail.com>
2024-01-12 08:25:32 -0500

    basics chain implementation. A template, then llm request, then ouput parser.

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are world class technical documentation writer."),
+    ("user", "Summarize this text: {input}")
+])
 
 llm = ChatOpenAI()
-response = llm.invoke("how can langsmith help with testing?")
+output_parser = StrOutputParser() # to see the output as a string, avoiding message object received from llm
+
+chain = prompt | llm | output_parser
+text = input("Enter text to summarize: ")
+response = chain.invoke({"input": text})
 print(response)
