e7e4df77c71d6e5a907eeff4665bba35be2b85f0
Camilo Figueroa <cigotete@gmail.com>
2024-01-15 20:24:16 -0500

    Testing 'plan and solve' reasoning strategy.

--- agents/agents_ui.py
+++ agents/agents_ui.py
@@ -1,13 +1,47 @@
+"""Question answering app in Streamlit.
+
+Originally based on this template:
+https://github.com/hwchase17/langchain-streamlit-template/blob/master/main.py
+
+"""
 import streamlit as st
 from langchain.callbacks import StreamlitCallbackHandler
-from agents.agents_basics import load_agent
 
-chain = load_agent()
-st_callback = StreamlitCallbackHandler(st.container())
+from agents_plan_n_solve import load_agent
+from utils import MEMORY
 
-if prompt := st.chat_input():
-  st.chat_message("user").write(prompt)
-  with st.chat_message("assistant"):
-    st_callback = StreamlitCallbackHandler(st.container())
-    response = chain.run(prompt, callbacks=[st_callback])
-    st.write(response)
+st.set_page_config(page_title="LangChain Question Answering", page_icon=":robot:")
+st.header("Ask a research question!")
+
+strategy = st.radio(
+    "Reasoning strategy",
+    ("plan-and-solve", "zero-shot-react", ))
+
+tool_names = st.multiselect(
+    'Which tools do you want to use?',
+    [
+        "google-search", "ddg-search", "arxiv",
+        "wikipedia", "python_repl", "pal-math",
+        "llm-math"
+    ],
+    ["ddg-search", "wikipedia"])
+if st.sidebar.button("Clear message history"):
+    MEMORY.chat_memory.clear()
+
+avatars = {"human": "user", "ai": "assistant"}
+for msg in MEMORY.chat_memory.messages:
+    st.chat_message(avatars[msg.type]).write(msg.content)
+
+assert strategy is not None
+agent_chain = load_agent(tool_names=tool_names, strategy=strategy)
+
+assistant = st.chat_message("assistant")
+if prompt := st.chat_input(placeholder="Ask me anything!"):
+    st.chat_message("user").write(prompt)
+    stream_handler = StreamlitCallbackHandler(assistant)
+    with st.chat_message("assistant"):
+        response = agent_chain.run({
+            "input": prompt,
+            "chat_history": MEMORY.chat_memory.messages
+        }, callbacks=[stream_handler]
+        )
